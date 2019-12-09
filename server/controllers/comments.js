7017613194636630aaefe0907d737c1ca56fe40f
Joey <reoko@protonmail.com>
2019-12-09 21:05:16 +0100

    up- and downvote comment routes

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -1,23 +1,91 @@
 const database = require("../database");
 
 const create = async (req, res) => {
-  const { parentComment, community_id, text } = req.body;
+  const { parentComment, post_id, text } = req.body;
   try {
     const newComment = await database("comments").insert(
       {
         parent_comment: parentComment,
+        post_id,
         user_id: req.user.id,
-        community_id,
         text
       },
       "*"
     );
-    res.status(201).send(newComment);
+    res.status(201).send(newComment[0]);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const upvote = async (req, res) => {
+  const { comment_id } = req.params;
+  const { post_id } = req.body;
+  try {
+    const commentArray = await database("comment_votes")
+      .select()
+      .where({ comment_id });
+    const commentVote = commentArray[0];
+    if (!commentVote) {
+      const newVote = await database("comment_votes").insert(
+        { user_id: req.user.id, comment_id, post_id, vote: true },
+        "*"
+      );
+      return res.send({ action: "create", vote: newVote });
+    }
+    if (commentVote.vote) {
+      const deletedVote = await database("comment_votes")
+        .del()
+        .where({ user_id: req.user.id, comment_id })
+        .returning("*");
+      return res.send({ action: "delete", vote: deletedVote });
+    } else {
+      const updatedVote = await database("comment_votes").update(
+        { vote: true, updated_at: database.fn.now() },
+        "*"
+      );
+      return res.send({ action: "update", vote: updatedVote });
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const downvote = async (req, res) => {
+  const { comment_id } = req.params;
+  const { post_id } = req.body;
+  try {
+    const voteArray = await database("comment_votes")
+      .select()
+      .where({ comment_id });
+    const commentVote = voteArray[0];
+    if (!commentVote) {
+      const newVote = await database("comment_votes").insert(
+        { user_id: req.user.id, comment_id, post_id, vote: false },
+        "*"
+      );
+      return res.send({ action: "create", vote: newVote });
+    }
+    if (!commentVote.vote) {
+      const deletedVote = await database("comment_votes")
+        .del()
+        .where({ user_id: req.user.id, comment_id })
+        .returning("*");
+      return res.send({ action: "delete", vote: deletedVote });
+    } else {
+      const updatedVote = await database("comment_votes").update(
+        { vote: false, updated_at: database.fn.now() },
+        "*"
+      );
+      return res.send({ action: "update", vote: updatedVote });
+    }
   } catch (e) {
     console.error(e);
   }
 };
 
 module.exports = {
-  create
+  create,
+  upvote,
+  downvote
 };
--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,8 +1,14 @@
 const { Router } = require("express");
 const { comments } = require("../controllers");
+const { protect } = require("../middlewares/authentications");
+const { schemas, check } = require("../middlewares/validations");
 
 const router = Router();
 
-router.route("/").post(comments.create);
+router
+  .route("/")
+  .post(protect, check(schemas.createComment, "body"), comments.create);
+router.post("/upvote/:comment_id", protect, comments.upvote);
+router.post("/downvote/:comment_id", protect, comments.downvote);
 
 module.exports = router;
